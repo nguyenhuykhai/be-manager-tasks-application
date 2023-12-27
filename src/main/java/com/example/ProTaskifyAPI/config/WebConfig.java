7ec3ae9fd54568b97a0364a7594089e32d0dd846
RankillerDY <rankillerdy@gmail.com>
2023-12-27 17:28:26 +0700

    add cors

---
+++ src/main/java/com/example/ProTaskifyAPI/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.ProTaskifyAPI.config;
+
+import java.util.Arrays;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig{
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+}
